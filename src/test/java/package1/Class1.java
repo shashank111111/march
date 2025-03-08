91792b92460f673bfb2e550e4d2265329d05e1b2
asus <asus@DESKTOP-LU2K2A6>
2025-03-08 15:44:58 +0530

    first commit

---
+++ src/test/java/package1/Class1.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	@Test
+	public void test1()
+	{
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
